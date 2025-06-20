2047d440a027b12a234c7913f5656aa4958b06da
rikyyy1607 <jhonlol122333@gmail.com>
2025-06-20 12:31:53 +0700

    edit config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 // vite.config.js
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/Classic-Quads-Barbers/", // GANTI dengan nama repo kamu
+    base: "/Classic-Quads-Barbers/", // GANTI dengan nama repo kamu
 });
