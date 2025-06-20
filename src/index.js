917b40d3c481e2490ee7bb82b4e0d87c33454ac9
rikyyy1607 <jhonlol122333@gmail.com>
2025-06-20 12:42:41 +0700

    change routes

--- src/index.js
+++ src/index.js
@@ -1,35 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Services from './pages/Services';
-import Nav from './components/Nav';
-import NotFound from './pages/Notfound';
-import Contact from './pages/Contact';
-import About from './pages/About';
-import Team from './pages/Team';
-import Reservations from './pages/Reservations';
-import Footer from './components/Footer';
-import TeamMember from './pages/TeamMember';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Team from "./pages/Team";
+import TeamMember from "./pages/TeamMember";
+import Reservations from "./pages/Reservations";
+import NotFound from "./pages/Notfound";
+
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <BrowserRouter basename="/nab-barber">
+  <HashRouter basename="/Classic-Quads-Barbers">
     <Nav />
     <Routes>
-      <Route path='/' Component={App}></Route>
-      <Route path='/services' Component={Services}></Route>
-      <Route path='/contact' Component={Contact}></Route>
-      <Route path='/about' Component={About}></Route>
-      <Route path='/team' Component={Team}></Route>
-      <Route path='/team/:id' Component={TeamMember}></Route>
-      <Route path='/reservations' Component={Reservations}></Route>
-      <Route path='*' Component={NotFound}></Route>
+      <Route path="/" element={<App />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/team" element={<Team />} />
+      <Route path="/team/:id" element={<TeamMember />} />
+      <Route path="/reservations" element={<Reservations />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
     <Footer />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 reportWebVitals();
